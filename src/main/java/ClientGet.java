b2b58e92e46c91c669a68bc67b48a9d938f42fdf
Mifou <michalskupienn@gmail.com>
2018-08-15 22:57:39 +0200

    ERC several examples

--- src/main/java/ClientGet.java
+++ src/main/java/ClientGet.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,24 +11,29 @@ import java.util.List;
 
 public class ClientGet {
 
+
+    private static CurrenciesExchangeRate createCurrenciesExchangeRate(Currencies first, Currencies second, String output) {
+        String split[] = output.split(":");
+        String exchangeValueString = split[1].replace("}", "");
+        double exchangeValue = Double.parseDouble(exchangeValueString);
+        return new CurrenciesExchangeRate(first, second, exchangeValue);
+
+    }
+
     public static void main(String[] args) throws IOException {
 
 
         Currencies currenciesTable[] = Currencies.values();
-        List<String> currenciesExchangeRateStringList = new ArrayList<>();
         List<CurrenciesExchangeRate> currenciesExchangeRateList = new ArrayList<>();
 
 
-        for (int j = 0; j < currenciesTable.length; j++) {
-            for (int i = 0; i < currenciesTable.length; i++) {
-                CurrenciesExchangeRate currenciesExchangeRate = new CurrenciesExchangeRate();
+        for (Currencies first : currenciesTable) {
+            for (Currencies second : currenciesTable) {
 
-                currenciesExchangeRate.setFirstCurrency(currenciesTable[j].toString());
-                currenciesExchangeRate.setSecondCurrency(currenciesTable[i].toString());
 
-                if (currenciesTable[i] != currenciesTable[j]) {
+                if (second != first) {
                     try {
-                        URL url = new URL("https://free.currencyconverterapi.com/api/v6/convert?q=" + currenciesTable[j] + "_" + currenciesTable[i] + "&compact=ultra");
+                        URL url = new URL("https://free.currencyconverterapi.com/api/v6/convert?q=" + first + "_" + second + "&compact=ultra");
                         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                         connection.setRequestMethod("GET");
                         connection.setRequestProperty("Accept", "application/json");
@@ -36,20 +43,12 @@ public class ClientGet {
                         }
 
                         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
                         String output;
                         while ((output = bufferedReader.readLine()) != null) {
-                            currenciesExchangeRateStringList.add(output);
-                            String split[] = output.split(":");
-                            String exchangeValueString = split[1].replace("}", "");
-                            double exchangeValue = Double.parseDouble(exchangeValueString);
-                            currenciesExchangeRate.setExchangeRate(exchangeValue);
-                            currenciesExchangeRateList.add(currenciesExchangeRate);
+                            currenciesExchangeRateList.add(createCurrenciesExchangeRate(first, second, output));
                         }
                         connection.disconnect();
                     } catch (MalformedURLException exception) {
-                        exception.printStackTrace();
-                    } catch (IOException exception) {
                         exception.printStackTrace();
                     }
                 }
@@ -57,7 +56,7 @@ public class ClientGet {
         }
         //Printing the result
         for (CurrenciesExchangeRate c : currenciesExchangeRateList) {
-            System.out.println(c.getFirstCurrency() + " " + c.getSecondCurrency() + " " + c.getExchangeRate());
+            System.out.println(c);
         }
     }
 }
